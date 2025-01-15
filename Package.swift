f74dd13aab055825e0beb5a63f590808ccec75cf
lucaArchidiacono <mail@lucaa.dev>
2025-01-15 21:39:04 +0100

    Move away from L10n plugin and instead build by myself

--- Package.swift
+++ Package.swift
@@ -17,19 +17,12 @@ let package = Package(
             targets: ["CasaZurigol10n"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CasaZurigol10n",
-            plugins: [
-                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
-            ]
+            name: "CasaZurigol10n"
         ),
     ]
 )
