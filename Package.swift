02091400052011c8c280db69a8cd3fe7b5957e4e
lucaArchidiacono <mail@archiluc.ch>
2025-07-15 22:09:49 +0200

    for the current moment exclude the xcstrings file

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CasaZurigol10n"
+            name: "CasaZurigol10n",
+            exclude: ["Resources/AppShortcuts.xcstrings", "Resources/InfoPlist.xcstrings", "Resources/Localizable.xcstrings"]
         ),
     ]
 )
